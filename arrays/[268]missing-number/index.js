c23d2385348a64126c15157dc4043a682037f4fb
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-11-24 22:14:01 -0300

    feat: initial commit

---
+++ arrays/[268]missing-number/index.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = (nums) => {};
+
+const nums = [3, 0, 1];
+
+const ok = missingNumber(nums);
+console.log("ok", ok);
