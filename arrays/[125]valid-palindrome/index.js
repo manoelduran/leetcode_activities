1e69bb19868b1515c2f89680246358e450bbefce
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-11-25 22:53:20 -0300

    feat: arrays - leetcode / question 268

--- arrays/[125]valid-palindrome/index.js
+++ arrays/[125]valid-palindrome/index.js
@@ -3,7 +3,23 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = (s) => {};
+var isPalindrome = (s) => {
+  s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
 
 const s = "A man, a plan, a canal: Panama";
 const result = isPalindrome(s);
+console.log("result", result);
+
+// Time complexity: O(n)
+// Space complexity: O(n)
