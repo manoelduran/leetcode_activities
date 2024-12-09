be46558a92dad84d8b9e6524e5aa4499b37ff444
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-12-08 21:14:10 -0300

    feat: initial commit island perimeter

---
+++ arrays/[463]island-perimeter/index.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function (grid) {};
+
+const grid = [
+  [0, 1, 0, 0],
+  [1, 1, 1, 0],
+  [0, 1, 0, 0],
+  [1, 1, 0, 0],
+];
+
+const result = islandPerimeter(grid);
+console.log("result", result);
