6917be3d276e7355c5418f2030cfbe4b97ee16fa
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-12-14 22:21:36 -0300

    feat: initial commit

---
+++ arrays/[2347]best-poker-hand/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {number[]} ranks
+ * @param {character[]} suits
+ * @return {string}
+ */
+var bestHand = function (ranks, suits) {};
+
+const ranks = [13, 2, 3, 1, 9];
+const suits = ["a", "a", "a", "a", "a"];
+
+const result = bestHand(ranks, suits);
+console.log(result);
