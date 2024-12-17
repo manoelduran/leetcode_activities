506c11a513ef960c88e3a9c7d430ab62c0435024
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-12-17 10:56:54 -0300

    feat: initial commit

---
+++ arrays/[2108]find-first-palindromic-string-in-the-array/index.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function (words) {};
+
+const words = ["abc", "car", "ada", "racecar", "cool"];
+
+const result = firstPalindrome(words);
+console.log(result);
