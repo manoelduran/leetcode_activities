ef4987c65c7dfab5c39790f3560e01a60d19c30a
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-12-18 20:11:07 -0300

    feat: array - leetocde / question 2108

--- arrays/[2108]find-first-palindromic-string-in-the-array/index.js
+++ arrays/[2108]find-first-palindromic-string-in-the-array/index.js
@@ -3,9 +3,33 @@
  * @param {string[]} words
  * @return {string}
  */
-var firstPalindrome = function (words) {};
+var firstPalindrome = function (words) {
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let isPalindrome = true;
+    let left = 0;
+    let right = word.length - 1;
+
+    while (left < right) {
+      if (word[left] !== word[right]) {
+        isPalindrome = false;
+        break;
+      }
+      left++;
+      right--;
+    }
+
+    if (isPalindrome) {
+      return word;
+    }
+  }
+  return "";
+};
 
 const words = ["abc", "car", "ada", "racecar", "cool"];
 
 const result = firstPalindrome(words);
 console.log(result);
+
+// Time Complexity is O(n * m)  so in worst case it will be O(n^2)
+// Space Complexity is O(1)
