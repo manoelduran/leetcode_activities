01fd98e5c4bfa48c5b172b5ea819a8a597c98a98
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-01-08 22:44:41 -0300

    feat: hash-table-leetcode / question 409

--- hash-table/[409]longest-palindrome/index.js
+++ hash-table/[409]longest-palindrome/index.js
@@ -3,7 +3,34 @@
  * @param {string} s
  * @return {number}
  */
-var longestPalindrome = function (s) {};
+var longestPalindrome = function (s) {
+  if (s.length < 2) return s.length;
+
+  const map = new Map();
+
+  // Count the frequency of each character
+  for (let i = 0; i < s.length; i++) {
+    map.set(s[i], (map.get(s[i]) || 0) + 1);
+  }
+
+  let length = 0;
+  let hasOdd = false;
+
+  for (const [char, count] of map) {
+    if (count % 2 === 0) {
+      length += count;
+    } else {
+      length += count - 1;
+      hasOdd = true;
+    }
+  }
+
+  if (hasOdd) {
+    length += 1;
+  }
+
+  return length;
+};
 
 const s = "abccccdd";
 const result = longestPalindrome(s);
