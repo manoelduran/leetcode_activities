abbc2c5c6358eb12e2dc03fc9f4dd3bab0cac384
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-01-11 21:13:35 -0300

    feat: initial commit

---
+++ hash-table/[389]find-the-difference/index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function (s, t) {};
+
+const s = "abcd";
+const t = "abcde";
+const result = findTheDifference(s, t);
+console.log(result);
