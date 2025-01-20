439652c5e6fc6af24201603e4390a22c0263881c
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-01-20 13:23:57 -0300

    feat: hash-table / question 1941

--- hash-table/[1941]check-if-all-characters-have-equal-number-of-occurrences/index.js
+++ hash-table/[1941]check-if-all-characters-have-equal-number-of-occurrences/index.js
@@ -3,8 +3,21 @@
  * @param {string} s
  * @return {boolean}
  */
-var areOccurrencesEqual = function (s) {};
+var areOccurrencesEqual = function (s) {
+  const map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    map.set(s[i], (map.get(s[i]) || 0) + 1);
+  }
+  const values = Array.from(map.values());
+
+  for (let i = 1; i < values.length; i++) {
+    if (values[i] !== values[0]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const s = "abacbc";
-const result = findTheDifference(s, t);
+const result = areOccurrencesEqual(s);
 console.log(result);
