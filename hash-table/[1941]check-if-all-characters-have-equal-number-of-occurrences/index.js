640ebd8a7365c179e6ab1c33d2da170f8cd0d047
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-01-19 23:46:40 -0300

    initial commit

---
+++ hash-table/[1941]check-if-all-characters-have-equal-number-of-occurrences/index.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {string} s
+ * @return {boolean}
+ */
+var areOccurrencesEqual = function (s) {};
+
+const s = "abacbc";
+const result = findTheDifference(s, t);
+console.log(result);
