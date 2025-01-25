6032c6d12fe27ce2a44066e0bb933ff3e57ccc1f
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-01-24 21:59:52 -0300

    feat: hash-table / question 2085

--- hash-table/[2085]count-common-words-with-one-occurence/index.js
+++ hash-table/[2085]count-common-words-with-one-occurence/index.js
@@ -4,7 +4,24 @@
  * @param {string[]} words2
  */
 var countWords = function (words1, words2) {
-  consolejson.log(words1, words2);
+  const map1 = new Map();
+  const map2 = new Map();
+
+  for (let word of words1) {
+    map1.set(word, (map1.get(word) || 0) + 1);
+  }
+  for (let word of words2) {
+    map2.set(word, (map2.get(word) || 0) + 1);
+  }
+
+  let count = 0;
+  for (let [key, value] of map1.entries()) {
+    if (value === 1 && map2.get(key) === 1) {
+      count++;
+    }
+  }
+
+  return count;
 };
 
 const words1 = ["leetcode", "is", "amazing", "as", "is"];
