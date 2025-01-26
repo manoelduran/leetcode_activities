5c0797e9d523bb824d0d0c5ab032ea4add79ed86
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-01-25 22:55:44 -0300

    initial commit

---
+++ hash-table/[2103]rings-and-rods/index.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {string} rings
+ * @return {number}
+ */
+var countPoints = function (rings) {};
+
+const rings = "B0B6G0R6R0R6G9";
+const result = countPoints(rings);
+console.log(result);
