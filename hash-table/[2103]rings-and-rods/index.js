eab201d8116cea203a7422f2fc4b31ad1dd7f5f2
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-01-27 23:30:10 -0300

    feat: for looping

--- hash-table/[2103]rings-and-rods/index.js
+++ hash-table/[2103]rings-and-rods/index.js
@@ -3,8 +3,16 @@
  * @param {string} rings
  * @return {number}
  */
-var countPoints = function (rings) {};
+var countPoints = function (rings) {
+  const map = new Map();
 
-const rings = "B0B6G0R6R0R6G9";
+  for (let i = 0; i < rings.length; i += 2) {
+    map.set(rings[i], rings[i + 1]);
+  }
+  console.log(map);
+};
+
+// const rings = "B0B6G0R6R0R6G9";
+const rings = "G4";
 const result = countPoints(rings);
 console.log(result);
