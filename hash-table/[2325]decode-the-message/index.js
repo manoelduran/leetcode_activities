326faac85567e1d4291856bed7fb9831ef20d397
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-02-11 13:02:45 -0300

    feat: decode the message solution

--- hash-table/[2325]decode-the-message/index.js
+++ hash-table/[2325]decode-the-message/index.js
@@ -6,10 +6,24 @@
  */
 var decodeMessage = function (key, message) {
   const map = new Map();
-  for (let i = 0; i < key.length; i++) {}
+  let alphabetIndex = 0;
+  for (const char of key) {
+    if (char !== " " && !map.has(char)) {
+      map.set(char, String.fromCharCode(97 + alphabetIndex));
+      alphabetIndex++;
+    }
+    if (alphabetIndex === 26) break;
+  }
+  return message
+    .split("")
+    .map((char) => map.get(char) || char)
+    .join("");
 };
 
-const key = "12345";
-const message = "abcde";
+const key = "the quick brown fox jumps over the lazy dog";
+const message = "vkbs bs t suepuv";
 const result = decodeMessage(key, message);
 console.log(result);
+
+// Time complexity: O(n)
+// Space complexity: O(n)
